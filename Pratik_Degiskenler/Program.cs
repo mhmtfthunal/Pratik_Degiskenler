b514b5553b10286b6021c10b1f1903c1f7742603
FatihNitro <FatihNitro@DESKTOP-KERR77G>
2025-05-11 19:45:23 +0300

    #Veri tiplerinde güncelleme sağlandı.

--- Pratik_Degiskenler/Program.cs
+++ Pratik_Degiskenler/Program.cs
@@ -1,41 +1,31 @@
-﻿Console.WriteLine("Lütfen aşağıdaki bilgileri giriniz:");
-
+﻿Console.WriteLine("Lütfen aşağıdaki bilgileri giriniz...");
 Console.Write("T.C Kimlik Numarası:");
-
 string tcKimlikNo = Console.ReadLine();
 
 Console.Write("Adı:");
-
 string adi = Console.ReadLine();
 
 Console.Write("Soyadı:");
-
 string soyAdi = Console.ReadLine();
 
 Console.Write("Telefon Numarası:");
-
-string telefonNumarasi = Console.ReadLine(); // Telefon numarası değerini metinsel değer olarak string aldım.
+string telefonNumarasi = Console.ReadLine(); // Matematiksel bir işlem olmayacağından string veri tipi kullandım.
 
 Console.Write("Yaş:");
-
-string yas = Console.ReadLine();
+byte yas = Convert.ToByte(Console.ReadLine());
 
 Console.Write("İlk Ürün Fiyatı:");
 
-int ilkFiyat = Convert.ToInt32(Console.ReadLine()); // int veri tipini kullandım. Sebebi ise sayısal değer ile matematiksel hesaplama yapmam gerekiyor. Yazılan string ifadeyi burada convert ediyoruz.
+double ilkFiyat = Convert.ToDouble(Console.ReadLine()); // double veri tipini kullandım. Sebebi ise sayısal değer ile matematiksel hesaplama yapmam gerekiyor. Yazılan string ifadeyi burada convert ediyoruz.
 
 Console.Write("İkinci Ürün Fiyatı:");
 
-int ikinciFiyat = Convert.ToInt32(Console.ReadLine()); // int veri tipini kullandım. Sebebi ise sayısal değer ile matematiksel hesaplama yapmam gerekiyor. Yazılan string ifadeyi burada convert ediyoruz.
-
-int urunFiyatToplam = ilkFiyat + ikinciFiyat;
-
-int puanMiktari = urunFiyatToplam * 10 / 100;
+double ikinciFiyat = Convert.ToDouble(Console.ReadLine()); // double veri tipini kullandım. Sebebi ise sayısal değer ile matematiksel hesaplama yapmam gerekiyor. Yazılan string ifadeyi burada convert ediyoruz.
+double urunFiyatToplam = ilkFiyat + ikinciFiyat;
+double puanMiktari = urunFiyatToplam * 10 / 100;
 
 Console.WriteLine("-----------------------------------------------");
 
 Console.WriteLine($"{tcKimlikNo} T.C. Kimlik Numaralı {adi} {soyAdi} isimli kişi için kayıt oluşturulmuştur.");
-
 Console.WriteLine($"{telefonNumarasi} telefon numarasına bildirim mesajı gönderilmiştir.");
-
 Console.WriteLine($"{urunFiyatToplam} toplam harcama karşılığı kazanılan %10 patika puan miktarı {puanMiktari} TL'dir.");
